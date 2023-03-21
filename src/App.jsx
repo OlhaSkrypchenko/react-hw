6b3d512f5476c3cbcc575bfc6fe036ef49ed9c9f
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-22 01:06:43 +0200

    added Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Card from "./components/card/Card";
 
-
+const url =
+  "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4772f7f8-ae41-4825-8c75-50ecd4ebd0ef/articles.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230321%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230321T095114Z&X-Amz-Expires=86400&X-Amz-Signature=4a3b0406735de5065f054067fb798f26f45a3640ae6445249354e487609872be&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22articles.json%22&x-id=GetObject";
 
 function App() {
-  
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsError(error.message);
+        setIsLoading(false);
+      });
+  }, [setData, setIsError]);
+
   return (
     <>
- 
+      {isLoading ? (<div className="loading">Loading ...</div>) : 
+        (
+          <div className="container">
+            {data.map((el) => (
+              <Card
+                key={el.id}
+                categoryId={el.category_id}
+                category={el.category.title}
+                image={el.image}
+                date={el.published_at}
+                title={el.title}
+                description={el.description}
+                avatar={el.author.avatar}
+                name={el.author.name}
+                position={el.author.position}
+              />
+            ))}
+          </div>
+        )
+      }
+      {isError.length > 0 && <p className="error">{isError}</p>}
     </>
-    
   );
 }
 
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,42 @@
+import './card.css';
+
+function getDateFormat(str) {
+    const date = new Date(str).toLocaleDateString();
+    const arr = date.split('/').map(el => {
+        if (el.length === 1) {
+            el = `0${el}`
+        }
+
+        return el;
+    });
+
+    const [month, day, year] = arr;
+
+    return `${day}.${month}.${year}`;
+}
+
+function Card({categoryId, image, category, title, date, description, avatar, name, position}) {
+    return (
+        <div className="card" id={categoryId}>
+            <span className="category">{category}</span>
+            <img src={image} alt="img" />
+            <div className="info">
+                <span className="date">{getDateFormat(date)}</span>
+                <h4 className="title">{title}</h4>
+                <p className="description">{description}</p>
+            </div>
+            <div className="author">
+                <img src={avatar} alt="avatar" className="avatar" />
+                <div className="author-info">
+                    <span className="author-name">
+                        <span>By </span>
+                        {name}
+                    </span>
+                    <span className="author-position">{position}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
