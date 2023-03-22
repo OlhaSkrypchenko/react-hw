84815bec7d1640032487a3a288faf4c7638efd18
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-22 21:51:50 +0200

    added filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import axios from "axios";
 import Card from "./components/card/Card";
 
-const url = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4772f7f8-ae41-4825-8c75-50ecd4ebd0ef/articles.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230322%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230322T131530Z&X-Amz-Expires=86400&X-Amz-Signature=ae786caf0f9bf766f967a91f7edae6c19561eaa9689cbe0cb9b232ede7d58f15&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22articles.json%22&x-id=GetObject';
+const url =
+  "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4772f7f8-ae41-4825-8c75-50ecd4ebd0ef/articles.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230322%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230322T131530Z&X-Amz-Expires=86400&X-Amz-Signature=ae786caf0f9bf766f967a91f7edae6c19561eaa9689cbe0cb9b232ede7d58f15&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22articles.json%22&x-id=GetObject";
 
-const topics = [
-  { category: "all", id: 0, isPicked: true },
-  { category: "adventure", id: 1, isPicked: false  },
-  { category: "travel", id: 2, isPicked: false },
-  { category: "fashion", id: 3, isPicked: false },
-  { category: "technology", id: 4, isPicked: false  },
-  { category: "branding", id: 5, isPicked: false },
-];
+const defaultFilters = { category: "all" };
 
 function App() {
-  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState("");
+
+  const [data, setData] = useState([]);
+  const [filters, setFilters] = useState(defaultFilters);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -33,22 +30,60 @@ function App() {
       });
   }, [setData, setIsError]);
 
+  const topics = [
+    { category: "all", id: 0 },
+    { category: "adventure", id: 1 },
+    { category: "travel", id: 2 },
+    { category: "fashion", id: 3 },
+    { category: "technology", id: 4 },
+    { category: "branding", id: 5 },
+  ];
+
+  const filterCards = useCallback(() => {
+    if (filters.category === "all") {
+      setFilteredData([...data]);
+    } else {
+      setFilteredData(
+        [...data].filter(
+          (el) => el.category.title.toLowerCase() === filters.category
+        )
+      );
+    }
+  }, [data, filters.category]);
+
+  useEffect(() => {
+    filterCards();
+  }, [filterCards]);
+
   return (
     <>
       {isLoading ? (
         <div className="loading">Loading ...</div>
-      ) : isError.length > 0 ? <p className="error">{isError}</p> : (
+      ) : isError.length > 0 ? (
+        <p className="error">{isError}</p>
+      ) : (
         <div className="container">
           <h1 className="main-title">Popular topics</h1>
           <div className="categories">
             {topics.map((el, index) => (
-              <span className={`category-title ${el.isPicked ? 'picked': ''}`} key={index} name={el.id}>
+              <span
+                className={`category-title ${
+                  el.category === filters.category ? "picked" : ""
+                }`}
+                key={index}
+                data-category-id={el.id}
+                onClick={() => {
+                  if (filters.category !== el.category) {
+                    setFilters({ category: el.category });
+                  }
+                }}
+              >
                 {el.category}
               </span>
             ))}
           </div>
           <div className="cards-container">
-            {data.map((el) => (
+            {filteredData.map((el) => (
               <Card
                 key={el.id}
                 categoryId={el.category_id}
@@ -65,7 +100,6 @@ function App() {
           </div>
         </div>
       )}
-      
     </>
   );
 }
