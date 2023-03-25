0d4214f0c5b401640371a32d24015c2f0f4b5c2b
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-26 01:26:05 +0200

    finished styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,83 @@
-import "./App.css";
+import './App.css';
 
-import ToDo from "./components/ToDo/ToDo";
-import { useState } from "react";
-import { useCallback } from "react";
+import ToDo from './components/ToDo/ToDo';
+import { useState } from 'react';
+import { useCallback } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
   const [uniqueId, setUniqueId] = useState(0);
 
-
-  const addNewTask = useCallback( ()=> {
-    if(newTask.length > 0) {
-      setUniqueId((prev=> ++prev));
-      setTasks((previousTasks) => [{task: newTask, id: uniqueId, isDone: false}, ...previousTasks]);
+  const addNewTask = useCallback(() => {
+    if (newTask.length > 0) {
+      setUniqueId((prev) => ++prev);
+      setTasks((previousTasks) => [
+        { task: newTask, id: uniqueId, isDone: false },
+        ...previousTasks,
+      ]);
     }
 
     setNewTask('');
-  }
-  , [newTask, uniqueId])
+  }, [newTask, uniqueId]);
 
   const handleChange = (e) => setNewTask(e.target.value);
   const handleKeyDown = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       addNewTask();
-   }
-  }
-
-  const handleRemoveTask= (id)=> {
-    setTasks((prev) => prev.filter(el => el.id !== id));
-  }
-   
-  const handleDone = (id)=> {
-    const doneTask = tasks.filter(el => el.id === id);
-    doneTask[0].isDone = !doneTask[0].isDone;
-    if (doneTask[0].isDone) {
-      setTasks((prev)=> [...prev.filter(el => el.id !== id), ...doneTask]);
-    } else {
-      setTasks((prev)=> [...doneTask, ...prev.filter(el => el.id !== id)]);
     }
-    
-    
-  }
+  };
 
-  
-  console.log(tasks);
+  const handleRemoveTask = (id) => {
+    setTasks((prev) => prev.filter((el) => el.id !== id));
+  };
+
+  const handleDone = (e, id) => {
+    e.stopPropagation();
+    const targetTask = tasks.find((el) => el.id === id);
+    const switchDoneTask = { ...targetTask, isDone: !targetTask.isDone };
+
+    if (switchDoneTask.isDone) {
+      setTasks((prev) => [
+        ...prev.filter((el) => el.id !== id),
+        switchDoneTask,
+      ]);
+    } else {
+      setTasks((prev) => [
+        switchDoneTask,
+        ...prev.filter((el) => el.id !== id),
+      ]);
+    }
+  };
 
   return (
-    <div className="container">
-      <h1 className="title">ToDo List</h1>
-      <div className="search">
+    <div className='container'>
+      <h1 className='title'>ToDo List</h1>
+      <div className='search'>
         <input
-          className="input"
-          type="text"
-          placeholder="Add new task"
-            value={newTask}
-          onChange={handleChange} 
+          className='input'
+          type='text'
+          placeholder='Add new task'
+          value={newTask}
+          onChange={handleChange}
           onKeyDown={handleKeyDown}
-          />
-        <button className='btn-add' onClick={addNewTask}>+</button>
-        
+        />
+        <button
+          className='btn-add'
+          onClick={addNewTask}>
+          +
+        </button>
       </div>
-      {tasks.map((el) => <ToDo task={el.task} key={el.id} id={el.id} handlerDelete={handleRemoveTask} handlerDone={handleDone} isChecked={el.isDone}/>)}
+      {tasks.map((el) => (
+        <ToDo
+          task={el.task}
+          key={el.id}
+          id={el.id}
+          handlerDelete={handleRemoveTask}
+          handlerDone={handleDone}
+          isChecked={el.isDone}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,15 +1,26 @@
-import './toDo.css'
+import './toDo.css';
 
 function ToDo({ task, id, handlerDelete, handlerDone, isChecked }) {
   return (
-    <div className="task-container">
-        <label className={`task ${isChecked? 'checked':''}`} id={id} onClick={()=> handlerDone(id)}>
-            <input className="checkbox" type="checkbox" />
-            <span className="task-description">{task}</span>
-        </label>
-        <button className="btn-remove" onClick={()=> handlerDelete(id)}>&#8722;</button>
+    <div className='task-container'>
+      <label
+        className={`task ${isChecked ? 'checked' : ''}`}
+        id={id}>
+        <input
+          className='checkbox'
+          type='checkbox'
+          onClick={(e) => {
+            handlerDone(e, id);
+          }}
+        />
+        <span className='task-description'>{task}</span>
+      </label>
+      <button
+        className='btn-remove'
+        onClick={() => handlerDelete(id)}>
+        &#8722;
+      </button>
     </div>
-    
   );
 }
 
