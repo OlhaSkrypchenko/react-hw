5c56d4b9acce229f8ce013d51cf8df31b8f1194d
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-30 16:05:58 +0300

    changed url for request

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import axios from "axios";
 import Card from "./components/card/Card";
 
-const url = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4772f7f8-ae41-4825-8c75-50ecd4ebd0ef/articles.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230322%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230322T131530Z&X-Amz-Expires=86400&X-Amz-Signature=ae786caf0f9bf766f967a91f7edae6c19561eaa9689cbe0cb9b232ede7d58f15&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22articles.json%22&x-id=GetObject';
+const url = '/db.json';
 
 const topics = [
   { category: "all", id: 0, isPicked: true },
